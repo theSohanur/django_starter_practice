b96fd00de1e08f58d49fff49e52f2a045971f15d
Sohanur Rahman <sohanur064@gmail.com>
2021-03-10 16:05:19 +0600

    first commit

---
+++ food/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # /food/
+    path('', views.index, name='index'),
+    # /food/1
+    path("<int:item_id>/", views.detail, name="detail"),
+    path('item/', views.item, name='index'),
+    # add item
+    path('add', views.create_item, name='create_item'),
+]
---
+++ food/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from django.http import HttpResponse
+from .models import Item
+from django.template import loader
+from .forms import ItemForm
+
+
+def index(request):
+    item_list = Item.objects.all()
+    template = loader.get_template("food/index.html")
+    context = {
+        'item_list': item_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def item(request):
+    return HttpResponse("<h2>Hello, This is item view.</h2>")
+
+
+def detail(request, item_id):
+    item_n = Item.objects.get(pk=item_id)
+    context = {
+        "item": item_n,
+    }
+
+    return render(request, 'food/detail.html', context)
+
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    return render(request, 'food/item_form.html', {'form': form})
