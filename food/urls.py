0509988b2dd0a23e1c7eed7d83fc97c9582f8b92
Sohanur Rahman <sohanur064@gmail.com>
2021-03-10 16:59:59 +0600

    Path problem solve

--- food/urls.py
+++ food/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("<int:item_id>/", views.detail, name="detail"),
     path('item/', views.item, name='index'),
     # add item
-    path('add', views.create_item, name='create_item'),
+    path('add/', views.create_item, name='create_item'),
 ]
