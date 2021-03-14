bdcff8f9c2d4fa867cd5564e127910bfb72043c9
Sohanur Rahman <sohanur064@gmail.com>
2021-03-14 14:28:13 +0600

    adding aditional fields on forms

--- users/views.py
+++ users/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
 # Create your views here.
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Welcome {username}, Your account is created.')
+            messages.success(request, f'Welcome {username}, Your account is created.')
             return redirect('index')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
+        # form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
