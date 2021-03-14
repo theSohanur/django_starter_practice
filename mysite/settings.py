974c3627cd8f6afd6bb2d24e296fcf03ed30b91b
Sohanur Rahman <sohanur064@gmail.com>
2021-03-14 07:13:40 +0600

    Creating User reg

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'food.apps.FoodConfig',
     'django.contrib.admin',
     'django.contrib.auth',
